a1d588987f28419e1be1ec3fc245afa5b5230edf
SanketMotewar <sanketmotewar49@gmail.com>
2023-07-14 14:17:28 +0530

    add: this is my first program

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, multiply;
+    
+  cout << "Enter two integers: ";
+  cin >> first_number >> second_number;
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  multiply = first_number * second_number;
+
+  // prints multiply 
+  cout << first_number << " * " <<  second_number << " = " << multiply;     
+
+  return 0;
+}
+
+   
